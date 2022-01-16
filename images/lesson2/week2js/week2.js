d3d28dc13fffec9ed3df5ee7543c7e5ae616b826
zannamai <joh21096@byui.edu>
2022-01-15 22:40:19 -0700

    margin

--- images/lesson2/week2js/week2.js
+++ images/lesson2/week2js/week2.js
@@ -1,2 +1,7 @@
-const lastmod = document.querySelector('#lastmod');
-lastmod.textContent = 'Page was last modified: ${document.lastModified}';
+document.addEventListener("DOMContentLoaded", function() { 
+    const yrSpan = document.querySelector('#year');
+    const currentYr = new Date().getFullYear();
+    yrSpan.textContent = currentYr;
+  });
+  let lastVisit = document.lastModified;
+  document.getElementById("updated").textContent = "Last Updated:" + lastVisit;
