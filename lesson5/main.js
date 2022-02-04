9280d6316819735a4d5613469ac789b377b2ba39
ZannaMai <97138655+ZannaMai@users.noreply.github.com>
2022-02-03 20:40:04 -0700

    java

--- lesson5/main.js
+++ lesson5/main.js
@@ -4,16 +4,19 @@ const buttonEnter = document.querySelector('button');
 
 buttonEnter.addEventListener('click', function()  {
   const myItem = inputVerse.value;
+
  const listItem = document.createElement('li');
-   const listText = document.createElement('span');
-     const listBtn = document.createElement('button');
+  const listText = document.createElement('span');
+  const listBtn = document.createElement('button');
+
   listItem.appendChild(listText);
-    listText.textContent = myItem;
-      listItem.appendChild(listBtn);
-        listBtn.textContent = `❌`;
-          list.appendChild(listItem);
+  listText.textContent = myItem;
+  listItem.appendChild(listBtn);
+  listBtn.textContent = `❌`;
+  list.appendChild(listItem);
+
   listBtn.onclick = function(e) {
-      list.removeChild(listItem);
-        };
-          inputVerse.focus();
-          });
+    list.removeChild(listItem);
+  };
+  inputVerse.focus();
+});
