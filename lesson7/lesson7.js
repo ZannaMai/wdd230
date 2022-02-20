c331c3e7150af7a6ab416aa2e0e9baa6d4a2bc03
ZannaMai <97138655+ZannaMai@users.noreply.github.com>
2022-02-19 19:26:04 -0700

    changes

--- lesson7/lesson7.js
+++ lesson7/lesson7.js
@@ -1,60 +1,27 @@
-// Headline JS
+const images = document.querySelectorAll("[data-src]");
 
-let start = -1;
-let end = start + 1;
-const headerClass = document.getElementById("headline");
-let headline = ["Zanna's Lazy Loading Page"]
+function preloadImage(img) {
+    const src = img.getAttribute("data-src");
+    if (!src) {
+        return;
+    }
 
+    img.src = src;
+}
 
-function headerPrint() {   
-    setTimeout(function() {
-        headerClass.innerHTML = headerClass.innerHTML + headline[0].slice(start,end);
-        start++;
-        end = start + 1;
-        if (start < headline[0].length) {
-            headerPrint();
+const imgOptions = {};
+
+const imgObserver = new IntersectionObserver((entries, imgObserver) => {
+    entries.forEach(entry => {
+        if (!entry.isIntersecting) {
+            return;
+        } else {
+            preloadImage(entry.target);
+            imgObserver.unobserve(entry.target);
         }
-        if (end === headline[0].length + 1) {
-            index = 1
-        }
-    }, 100);
-  } 
+    })
+}, imgOptions);
 
-headerPrint();
-
-// Lazy loading
-
-let imagesToLoad = document.querySelectorAll('img[data-src]');
-
-const imageOptions = {
-    threshold: 0,
-    rootMargin: '0px 0px 50px 0px'
-};
-
-const loadImages = (image) => {
-    image.setAttribute("src", image.getAttribute('data-src'));
-    image.onload = () => {
-        image.removeAttribute('data-src');
-    };
-};
-
-
-if('IntersectionObserver' in window) {
-    const observer = new IntersectionObserver((items, observer) => {
-      items.forEach((item) => {
-        if(item.isIntersecting) {
-          loadImages(item.target);
-          observer.unobserve(item.target);
-        }
-      });
-    }, imageOptions);
-
-
-    imagesToLoad.forEach((img) => {
-      observer.observe(img);
-    });
-  } else {
-    imagesToLoad.forEach((img) => {
-      loadImages(img);
-    });
-  }
+images.forEach(image => {
+    imgObserver.observe(image);
+})
