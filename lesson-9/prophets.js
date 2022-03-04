39e54db12212d512d6ca16eaef5bacba2145f148
ZannaMai <97138655+ZannaMai@users.noreply.github.com>
2022-03-03 20:06:16 -0700

    yes

--- lesson-9/prophets.js
+++ lesson-9/prophets.js
@@ -7,28 +7,29 @@ fetch(requestURL)
   })
   .then(function (jsonObject) {
     console.table(jsonObject);  // temporary checking for valid response and data parsing
+    const prophets = jsonObject['prophets'];
+    prophets.forEach(displayProphets);
   });
-  const prophets = jsonObject['prophets'];
-  prophets.forEach(displayProphets);
 
-function displayProphets(prophet) {
-    // Create elements to add to the document
-    let card = document.createElement('section');
-    let h2 = document.createElement('h2');
-    let portrait = document.createElement('img');
-  
-    // Change the textContent property of the h2 element to contain the prophet's full name
-    h2.textContent = prophet.name + ' ' + prophet.lastname;
-  
-    // Build the image attributes by using the setAttribute method for the src, alt, and loading attribute values. (Fill in the blank with the appropriate variable).
-    ____.setAttribute('src', prophet.imageurl);
-    ____.setAttribute('alt', 'Portait of ' + prophet.name + ' ' + prophet.lastname);
-    ____.setAttribute('loading', 'lazy');
-  
-    // Add/append the section(card) with the h2 element
+  function displayProphets(prophet) {
+    // create elements to add to the document
+    let card = document.createElement("section");
+    let h2 = document.createElement("h2")
+    let portrait = document.createElement("img")
+    let info = document.createElement("p")
+// add the prophets name to the h2 textcontent
+    h2.textContent = prophet.name + " " + prophet.lastname;
+    info.innerHTML = `Date of Birth: ${prophet.birthdate} <br> Place of Birth: ${prophet.birthplace} `
+    portrait.setAttribute('src', prophet.imageurl);
+    portrait.setAttribute('alt', `Portait of ${prophet.name} ${prophet.lastname} - ${prophet.order}th Latter-day President`);
+    portrait.setAttribute('loading', 'lazy')
+
+// appent to the section(card)
     card.appendChild(h2);
+    card.appendChild(info)
     card.appendChild(portrait);
-  
-    // Add/append the existing HTML div with the cards class with the section(card)
+
+// add to existing HTML div
     cards.appendChild(card);
-  }
+    
+}
