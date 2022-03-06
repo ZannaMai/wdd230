d1ea0b6dd843114ec7e587f6aa9042112fd8f847
ZannaMai <97138655+ZannaMai@users.noreply.github.com>
2022-03-05 22:28:06 -0700

    hopin

--- wdd230/chamber/js/directory.js
+++ wdd230/chamber/js/directory.js
@@ -19,15 +19,15 @@ fetch(requestURL)
     let info = document.createElement("p")
 // add the companies name to the h2 textcontent
     h2.textContent = company.name + " " + company.address;
-    info.innerHTML = `Date of Birth: ${company.phonenumber} <br> Place of Birth: ${company.birthplace} `
-    portrait.setAttribute('src', company.imageurl);
-    portrait.setAttribute('alt', `Portait of ${company.name} ${company.address} - ${company.order}th Latter-day President`);
-    portrait.setAttribute('loading', 'lazy')
+    info.innerHTML = `Phone Number: ${company.phonenumber} <br> Website: ${company.website} `
+    favicon.setAttribute('src', company.imageurl);
+    favicon.setAttribute('alt', `${company.name} ${company.address}`);
+    favicon.setAttribute('loading', 'lazy')
 
 // appent to the section(card)
     card.appendChild(h2);
     card.appendChild(info)
-    card.appendChild(portrait);
+    card.appendChild(favicon);
 
 // add to existing HTML div
     cards.appendChild(card);  
